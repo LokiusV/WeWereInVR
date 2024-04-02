f6d757f98a3822019971716170c5d954acd2b3e5
LokiusV <161963874+LokiusV@users.noreply.github.com>
2024-04-02 23:29:16 +0200

    Update SnapTurnProvider.cs

--- SteamVRInject/SnapTurnProvider.cs
+++ SteamVRInject/SnapTurnProvider.cs
@@ -7,9 +7,65 @@ using UnityEngine;
 
 namespace WeWereHereVR
 {
-    //TODO: Add Snap Turn
+    //Attention: requires cooldown to work
     public class SnapTurnProvider
     {
+        //positive
+        public static bool SnapRight()
+        {
+            if (!Var.snapDone && Input.GetAxis("joy_1_axis_3") >= 0.7f)
+            {
+                Var.snapDone = true;
+                return true;
+
+            }
+            if (Var.snapDone && Input.GetAxis("joy_1_axis_3") <= 0.25f)//&& Input.GetAxis("joy_1_axis_3")>=-0.25f)
+            {
+                Var.snapDone = false;
+                return false;
+            }
+            return false;
+
+        }
+
+        //negative
+        public static bool SnapLeft()
+        {
+            if (!Var.snapDone && Input.GetAxis("joy_1_axis_3") <= -0.7f)
+            {
+                Var.snapDone = true;
+                return true;
+
+            }
+            if (Var.snapDone && Input.GetAxis("joy_1_axis_3") >= -0.25f)//&& Input.GetAxis("joy_1_axis_3") <= 0.25f) //wayyyyy too high to fix this shit rn
+            {
+                Var.snapDone = false;
+                return false;
+            }
+            return false;
+
+        }
+        public static void Snap()
+        {
+            if(SnapRight())
+            {
+                try
+                {
+                    Var.playerGameObject.transform.Rotate(0, 45, 0);
+                }
+                catch (Exception e) { Debug.LogException(e); }
+            }
+            if (SnapLeft())
+            {
+                try
+                {
+                    Var.playerGameObject.transform.Rotate(0, -45, 0);
+                }
+                catch (Exception e) { Debug.LogException(e); }
+            }
+
+        }
+
 
     }
 }
